395b47ed51794b552893587e51ad54eeeb6e321c
Maksym Rybalko <maxymrybalko2005@gmail.com>
2024-02-19 20:10:51 +0200

    Modified main function to display time

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"time"
 )
 
 func main() {
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		currentTime := map[string]string{
+			"time": time.Now().Format(time.RFC3339),
+		}
+		jsonResponse, err := json.Marshal(currentTime)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonResponse)
+	})
 	http.ListenAndServe(":8795", nil)
 }
